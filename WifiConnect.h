ktos/Mokosh

commit 66c56c767780e8470d19b70a281da22fcdfb720e

Split WiFi connection and host string configuration (BREAKING CHANGE)

WifiConnect.h
@@ -5,12 +5,17 @@
 
 #include "Arduino.h"
 
-// tries connecting to the wireless network, configures internal host
-// name by chip id
+// tries connecting to the wireless network
 bool WiFi_connect(const char* ssid, const char* password, const char* prefix);
 
 // returns internal host name
 char* WiFi_getHostString();
+
+// configures internal host name by chip id
+void WiFi_init(const char* prefix);
+
+// returns if WiFi has been initialized
+bool WiFi_isInit();
 
 #endif
 

WifiConnect.cpp
@@ -4,10 +4,19 @@
 
 char hostString[16] = { 0 };
 
-bool WiFi_connect(const char* ssid, const char* password, const char* prefix)
+void WiFi_init(const char* prefix) {
+    sprintf(hostString, "%s_%06X", prefix, ESP.getChipId());
+}
+
+bool WiFi_isInit()
+{
+    return (hostString[0] != 0);
+}
+
+bool WiFi_connect(const char* ssid, const char* password)
 {
 	WiFi.enableAP(0);
-	sprintf(hostString, "%s_%06X", prefix, ESP.getChipId());
+
 	Debug_print(DLVL_INFO, "WIFI", hostString);
 	WiFi.hostname(hostString);
 

examples/WifiMqtt/WifiMqtt.ino
@@ -7,7 +7,8 @@ void setup() {
 
     // connects to the WiFi network called yourssid with password yourpassword
     // setting hostname to Mokosh_XXXXX
-    WiFi_connect("yourssid", "yourpassword", "Mokosh");
+    WiFi_init("Mokosh");
+    WiFi_connect("yourssid", "yourpassword");
 
     // will connect to MQTT broker at 192.168.1.10 at port 1883
     Mqtt_setup("192.168.1.10", 1883);

commit e5d766313132f6f8fdd3ff9d3c598974b36dc733

Fix error in header file introduced in 66c56c767780e84

WifiConnect.h
@@ -6,7 +6,7 @@
 #include "Arduino.h"
 
 // tries connecting to the wireless network
-bool WiFi_connect(const char* ssid, const char* password, const char* prefix);
+bool WiFi_connect(const char* ssid, const char* password);
 
 // returns internal host name
 char* WiFi_getHostString();